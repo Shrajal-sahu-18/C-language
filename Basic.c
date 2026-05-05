95ab64cea383052a49fa645132c4597b8ff32dd4
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-05 23:33:48 +0530

    Varriable

--- Basic.c
+++ Basic.c
@@ -1,5 +1,20 @@
+// # include<stdio.h>
+// int main(){
+//     printf("Hello World");
+//     return 0;
+// }
+
+// Varriable
 # include<stdio.h>
 int main(){
-    printf("Hello World");
-    return 0;
+    int age = 18;
+   char star = '*';
+   float pi = 3.14;
+
+   int a = 10;
+   int Final_price = 100;
+
+   return 0;
+
+
 }
