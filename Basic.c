d78c149d92e27ac42535661d95f41b16a17e5bed
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-07 08:10:13 +0530

    Print statement

--- Basic.c
+++ Basic.c
@@ -5,19 +5,29 @@
 // }
 
 // Varriable
-# include<stdio.h>
-int main(){
-    int age = 18;
-   char star = '*';
-   float pi = 3.14;
+// # include<stdio.h>
+// int main(){
+//     int age = 18;
+//    char star = '*';
+//    float pi = 3.14;
 
-   int a = 10;
-   int Final_price = 100;
+//    int a = 10;
+//    int Final_price = 100;
 
-   return 0;
+//    return 0;
 
 
-}
+// }
 
 // comment
 /* multi line comment*/
+
+#include<stdio.h>
+int main(){
+    int age = 22;
+    printf("age is %d \n",age);
+    float pi = 3.14;
+    printf("value of pi %f \n",pi);
+    char star = '*';
+    printf("star is %c \n",star);
+}
