ffec660a730d0eb92306390727195132b1c74a75
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-10 22:38:23 +0530

    valiad statement in c

--- Basic.c
+++ Basic.c
@@ -22,20 +22,35 @@
 // comment
 /* multi line comment*/
 
-#include<stdio.h>
-int main(){
-    int age = 22;
-    printf("age is %d \n",age);
-    float pi = 3.14;
-    printf("value of pi %f \n",pi);
-    char star = '*';
-    printf("star is %c \n",star);
-}
+// #include<stdio.h>
+// int main(){
+//     int age = 22;
+//     printf("age is %d \n",age);
+//     float pi = 3.14;
+//     printf("value of pi %f \n",pi);
+//     char star = '*';
+//     printf("star is %c \n",star);
+// }
 
 // Input
-#include<stdio.h>
-int main() {
-    int age ;
-    printf("Enter age:");
-    scanf("%d",&age);
-    printf("Age is : %d \n",age);
+// #include<stdio.h>
+// int main() {
+//     int age ;
+//     printf("Enter age:");
+//     scanf("%d",&age);
+//     printf("Age is : %d \n",age);
+
+
+// valid statement
+
+// #include<stdio.h>
+// int main(){
+//     int a = 22;
+//     int b = a;
+//     int c = b * 6;
+//     int d = 1,e;
+//     int a,b,c;
+//     a = b = c = 1;
+//     int z= 1,g = 2;
+//     return 0;
+// }
