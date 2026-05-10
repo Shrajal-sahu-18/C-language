363d30eb31b2c437f9f4409c2ecb72694e92c55a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-10 22:29:46 +0530

    Area of sqare

--- question.c
+++ question.c
@@ -1,11 +1,23 @@
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     printf("Enter a:");
+//     scanf("%d",&a);
+//     printf("Enter b :");
+//     scanf("%d",&b);
+//     int sum = a+b;
+//     printf("Sum of Two number : %d",sum);
+
+// }
+
+
 #include<stdio.h>
 int main(){
-    int a,b;
-    printf("Enter a:");
-    scanf("%d",&a);
-    printf("Enter b :");
-    scanf("%d",&b);
-    int sum = a+b;
-    printf("Sum of Two number : %d",sum);
+    int side;
+    printf("Enter side of sqare:");
+    scanf("%d",&side);
+    int sqare = side * side;
+    printf("Area of a sqare: %d",sqare);
+    return 0 ;
 
 }
