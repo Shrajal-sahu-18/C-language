b01d5138cab80cfc92c444722b6056b55314fd54
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-20 22:31:26 +0530

    avg of three number

--- question.c
+++ question.c
@@ -97,3 +97,18 @@ int main (){
     printf("%d \n",x>9 && x<100);
     return 0;
 }
+
+
+#include<stdio.h>
+int main (){
+    int a,b,c;
+    printf("Enter number a:");
+    scanf("%d",&a);
+    printf("Enter number b:");
+    scanf("%d",&b);
+    printf("Enter number c:");
+    scanf("%d",&c);
+    float avg = (a+b+c)/3.0 ;
+    printf("avarage of Three number %f \n",avg);
+    return 0;
+}
