bb4f5e654dfe90c4cfb4212a4a49b7153fbb2c2a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-20 22:33:14 +0530

    checking digits

--- question.c
+++ question.c
@@ -124,3 +124,14 @@ int main(){
     printf("avarage = %f",avg);
     return 0;
 }
+
+
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    printf("%d",ch >= '0'&& ch <='9');
+    return 0;
+ 
+}
