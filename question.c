141a63349acb47cdbe4d22800f9bc0069290623f
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-20 22:37:43 +0530

    conditional statement

--- question.c
+++ question.c
@@ -166,3 +166,22 @@ int main(){
     printf("thankyou");
     return 0;
 }
+
+
+
+
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Enter age:");
+    scanf("%d",&age);
+    if (age >= 18){
+        printf("Adult \n");
+    }
+    else if (age >13 && age <18){
+        printf("teenager \n");
+    }
+    else{
+        printf("Child");
+    }
+}
