acea8b34eab8c63cb16acf025cbfb4f7c5b962e4
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-27 23:07:25 +0530

    question

--- question.c
+++ question.c
@@ -213,6 +213,13 @@ int main() {
 
     printf("Enter a number: ");
     scanf("%d", &num);
+    if(num > 0) {
+        printf("Positive Number");
+    } else if(num < 0) {
+        printf("Negative Number");
+    } else {
+        printf("Zero");
+    }
 
    
 
