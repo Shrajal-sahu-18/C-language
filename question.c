141e5128f9fee58eeea9beaaed719b813775c3d5
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-27 23:08:07 +0530

    simple calculator

--- question.c
+++ question.c
@@ -225,3 +225,39 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf("%c", &op);
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    switch(op) {
+        case '+':
+            printf("Result = %d", a + b);
+            break;
+
+        case '-':
+            printf("Result = %d", a - b);
+            break;
+
+        case '*':
+            printf("Result = %d", a * b);
+            break;
+
+        case '/':
+            printf("Result = %d", a / b);
+            break;
+
+        default:
+            printf("Invalid Operator");
+    }
+
+    return 0;
+}
