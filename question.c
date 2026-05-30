4df7aef18bcbeedafc5e10a7c34e0912934d50f4
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-30 22:57:54 +0530

    conditional statement

--- question.c
+++ question.c
@@ -360,3 +360,21 @@ int main() {
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("Even Number");
+    else
+        printf("Odd Number");
+
+    return 0;
+}
