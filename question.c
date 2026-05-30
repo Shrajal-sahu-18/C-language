6a678ae5bb04266121962a77b4d229652e20fd02
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-30 22:54:12 +0530

    ternery operator

--- question.c
+++ question.c
@@ -329,3 +329,16 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20;
+
+    int max = (a > b) ? a : b;
+
+    printf("Maximum = %d\n", max);
+
+    return 0;
+}
