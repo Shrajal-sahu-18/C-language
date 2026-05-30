abb4a92ff1f1679b790b8f8763fd59d46ee582be
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-30 22:49:38 +0530

    conditional statement

--- question.c
+++ question.c
@@ -261,3 +261,16 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+
+    if (age >= 18) {
+        printf("You are an adult.\n");
+    }
+
+    return 0;
+}
