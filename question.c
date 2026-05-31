beca09fa35a46908a94413aec6d6cb6b099ff804
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-31 22:55:06 +0530

    code

--- question.c
+++ question.c
@@ -396,3 +396,17 @@ int main() {
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+
+    return 0;
+}
