2aa715d976f85c110431f1abf6f537467e67f548
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-11 23:34:20 +0530

    Question on conditional statement

--- question.c
+++ question.c
@@ -444,3 +444,11 @@ int main(){
     int marks;
     printf("Enter marks: ");
     scanf("%d",&marks);
+        if (marks > 30){
+        printf("Pass");
+    } else{
+        printf("Fail");
+    }
+    return 0;
+    
+}
