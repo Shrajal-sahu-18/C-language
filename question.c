e53bbcb03bbae3fda1b186799c62fb413d8c175a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-16 22:33:19 +0530

    Sum of n natural number using for loop

--- question.c
+++ question.c
@@ -498,4 +498,16 @@ int main(){
 //     int n ;
 //     printf("Enter number:");
 //     scanf("%d",&n);
+//     int sum = 0;
+//     for(int i = 1,j = n;i<=n && j>=1; i++,j--){
+//         sum+=i;
+//         printf("%d\n",j);
+//     }
+//     // for(int i =1;i<=n;i++){
+//     //     sum +=i;
+//     // }
+//     printf("sum is:%d\n",sum);
 
+//     // for(int i = n;i>=1;i--){
+//     //     printf("%d\n",i);
+//     // }
