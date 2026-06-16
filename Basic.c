6fc1fd8abcbab47c02a9ed5895e39aa22c575db9
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-16 22:30:28 +0530

    Do while loop

--- Basic.c
+++ Basic.c
@@ -266,3 +266,17 @@ int main(){
 //     return 0;
 
 // }
+
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int i = 5;
+//     do{
+//         printf("%d\n",i);
+//         i--;
+//     }while(i>=1);
+//     return 0;
+
+// }
