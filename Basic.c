9ed62399525b5e5cac1c30f8cab81389121e4d0a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-16 22:37:05 +0530

    Break Statement

--- Basic.c
+++ Basic.c
@@ -280,3 +280,15 @@ int main(){
 //     return 0;
 
 // }
+
+// break statement
+// #include<stdio.h>
+// int main(){
+//     for(int i =1;i<=5;i++){
+//         if (i == 3){
+//             break;
+//         }
+//         printf("%d\n",i);
+//     }
+//     printf("End");
+// }
