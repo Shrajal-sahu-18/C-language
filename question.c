94ead09d46ce5f70b91351999b18adebe92216d5
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-17 23:52:53 +0530

    factorial of n number

--- question.c
+++ question.c
@@ -565,3 +565,16 @@ int main(){
     }
     return 0;
 }
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter number:");
+    scanf("%d",&n);
+    int fact = 1;
+    for (int i = 1;i<=n;i++){
+        fact *=i;
+    }
+    printf("Factorial of %d is:%d",n,fact);
+    return 0;
+}
