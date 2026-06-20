52892b93f6077b90b76a4a79d02cf17690c532d2
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-20 22:49:52 +0530

    print reverse number 10 to 1

--- question.c
+++ question.c
@@ -581,6 +581,12 @@ int main(){
 
 // #include<stdio.h>
 // int main(){
-//     
+//     int n;
+//     printf("Enter number:");
+//     scanf("%d",&n);
+//     for(int i = 10;i>=1;i--){
+//         int k = n * i;
+//         printf("%d\n",k);
+//     }
 //     return 0;
 // }  
