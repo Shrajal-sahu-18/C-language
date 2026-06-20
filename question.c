d796cf68bdc84d1d8ed18fef90f3de2baa23aaef
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-20 22:51:44 +0530

    sum of all number

--- question.c
+++ question.c
@@ -594,6 +594,11 @@ int main(){
 
 #include<stdio.h>
 int main(){
+        int sum = 0;
+    for(int i = 5;i<=50;i++){
+        sum += i;
+    }
+    printf("sum is :%d",sum);  
         
     return 0;
 
