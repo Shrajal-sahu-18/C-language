93028583ee5887e4befce0d13a05623f7caa3f93
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-20 22:53:45 +0530

    break 3

--- question.c
+++ question.c
@@ -606,5 +606,12 @@ int main(){
 
 // #include<stdio.h>
 // int main(){
+    // for(int i =1;i<=5;i++){
+//         if (i == 3){
+//             break;
+//         }
+//         printf("%d\n",i);
+//     }
+//     printf("End");
 //    return 0;
 // }
