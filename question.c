9e0c96e33381c873a661c70bd1dfe590cf8c47e7
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-30 22:46:51 +0530

    Check if a string is a palindrome

--- question.c
+++ question.c
@@ -809,3 +809,11 @@ int main() {
 int isPalindrome(char str[]) {
     int left = 0;
     int right = strlen(str) - 1;
+        while (left < right) {
+        if (str[left] != str[right])
+            return 0;
+        left++;
+        right--;
+    }
+    return 1;
+}
