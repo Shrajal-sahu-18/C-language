53f4ee85fbcdeebe98dbeae98453ee363120840d
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-30 22:40:58 +0530

    function to check number is prime

--- question.c
+++ question.c
@@ -757,3 +757,19 @@ int main()
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+// Function to check if a number is prime
+int isPrime(int n) {
+    if (n <= 1)
+        return 0;  // not prime
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return 0;  // divisible, not prime
+    }
+    return 1;  // prime
+}
