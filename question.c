b327173e87b07b2c4853a075bce86324f64145cc
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-30 22:47:36 +0530

    Check if a string is a palindrome

--- question.c
+++ question.c
@@ -821,3 +821,10 @@ int main() {
     char str[100];
     printf("Enter a string: ");
     scanf("%s", str);
+        if (isPalindrome(str))
+        printf("%s is a palindrome.\n", str);
+    else
+        printf("%s is not a palindrome.\n", str);
+
+    return 0;
+}
