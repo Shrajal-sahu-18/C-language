77b1369880aba32a95fa4f80a869c4b74b264b77
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-03 22:01:46 +0530

    sqare of a number

--- question.c
+++ question.c
@@ -855,3 +855,14 @@ int square(int n)
 {
     return n * n;
 }
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Square = %d", square(num));
+
+    return 0;
+}
