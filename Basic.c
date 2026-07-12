eeee9a3fbc11aff861f65a8bf4d3572683d1b462
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-12 21:46:53 +0530

    Factorial using function

--- Basic.c
+++ Basic.c
@@ -488,3 +488,15 @@ int main()
 
     return 0;
 }
+
+#include <stdio.h>
+
+int factorial(int n)
+{
+    int i, fact = 1;
+
+    for (i = 1; i <= n; i++)
+        fact = fact * i;
+
+    return fact;
+}
