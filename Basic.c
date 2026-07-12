f05284f77f63ece2366955ff981a6211cf98eb44
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-12 21:48:49 +0530

    Square of n number

--- Basic.c
+++ Basic.c
@@ -520,3 +520,14 @@ int square(int n)
 {
     return n * n;
 }
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Square = %d", square(num));
+
+    return 0;
+}
