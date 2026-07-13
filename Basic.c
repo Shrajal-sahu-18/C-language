a2043e6f3c0fc2fbc9f66bc849a614269de3941c
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-13 22:23:51 +0530

    Swap of two number

--- Basic.c
+++ Basic.c
@@ -572,3 +572,14 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
+int main()
+{
+    int x = 10, y = 20;
+
+    swap(&x, &y);
+
+    printf("x = %d\n", x);
+    printf("y = %d", y);
+
+    return 0;
+}
