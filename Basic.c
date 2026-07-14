fe0df0519a90cef3ca41f0688f228b65f39b3cb9
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-14 22:05:47 +0530

    Maximun of three number

--- Basic.c
+++ Basic.c
@@ -631,3 +631,21 @@ int main()
 
     return 0;
 }
+
+
+
+
+#include <stdio.h>
+
+int minimum(int a, int b, int c)
+{
+    int min = a;
+
+    if (b < min)
+        min = b;
+
+    if (c < min)
+        min = c;
+
+    return min;
+}
