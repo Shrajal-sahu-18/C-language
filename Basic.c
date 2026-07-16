acc0fd9f4ea2d0076fbe1f4368345cb21bd52440
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-16 23:03:03 +0530

    sum of digits

--- Basic.c
+++ Basic.c
@@ -689,3 +689,18 @@ int main()
 
     return 0;
 }
+
+#include <stdio.h>
+
+int sumDigits(int n)
+{
+    int sum = 0;
+
+    while(n != 0)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+
+    return sum;
+}
