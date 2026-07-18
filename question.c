935d57ec298ea892ac395e6acb7669dc0704e263
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-18 21:59:33 +0530

    factorial using recursion

--- question.c
+++ question.c
@@ -1118,3 +1118,13 @@ int main() {
     printf("\n");
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
