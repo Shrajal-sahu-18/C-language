fa8561d04b3f9250f4e276233ce572b59bcaa812
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-18 22:00:37 +0530

    fibonacci using recursion

--- question.c
+++ question.c
@@ -1141,3 +1141,15 @@ int main() {
     
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
