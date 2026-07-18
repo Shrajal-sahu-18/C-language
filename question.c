2a59fa6b38244dcffbc6f5d4980eb8a34679f767
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-18 22:01:38 +0530

    fibonacci using recursion

--- question.c
+++ question.c
@@ -1153,3 +1153,16 @@ int fibonacci(int n) {
         return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
+int main() {
+    int n, i;
+    
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+    
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", fibonacci(i));
+    
+    printf("\n");
+    return 0;
+}
