9d19f9ee30a313f9566ef7aa75e78f9e80ecc961
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-19 22:20:49 +0530

    pailandrome

--- question.c
+++ question.c
@@ -1205,3 +1205,20 @@ int countDigits(int n)
     }
     return count;
 }
+
+
+
+#include <stdio.h>
+
+int reverse(int n)
+{
+    int rev = 0;
+
+    while(n != 0)
+    {
+        rev = rev * 10 + n % 10;
+        n = n / 10;
+    }
+
+    return rev;
+}
