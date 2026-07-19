a846c47f2ba9a037791bcaeed7f743106b21d685
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-19 22:19:16 +0530

    count digits

--- question.c
+++ question.c
@@ -1189,3 +1189,17 @@ int main()
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+int countDigits(int n)
+{
+    int count = 0;
+
+    while(n != 0)
+    {
+        count++;
+        n = n / 10;
+    }
