b345b9a670f178b3f8a371b61db7cde51cf08148
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-21 23:31:42 +0530

    check char or digit

--- question.c
+++ question.c
@@ -1300,3 +1300,10 @@ int main()
 
 int main() {
     char ch;
+     printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        printf("Alphabet");
+    else
+        printf("Not an Alphabet");
