c56c7bd9a73e1e211315b7777c14c23dd179adb1
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-22 21:19:13 +0530

    alphabet or not

--- question.c
+++ question.c
@@ -1329,3 +1329,10 @@ int main() {
 
 int main() {
     char ch;
+        printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        printf("Alphabet");
+    else
+        printf("Not an Alphabet");
